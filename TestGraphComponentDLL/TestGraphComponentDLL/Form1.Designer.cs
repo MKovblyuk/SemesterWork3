fbdc278f638c32dfb092cbb8b0c33d7660c01004
Mykola Kovblyuk <mkvblyuk@gmail.com>
2021-04-20 22:20:39 +0300

    commit test form

---
+++ TestGraphComponentDLL/TestGraphComponentDLL/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace TestGraphComponentDLL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnIsConnectedGraph = new System.Windows.Forms.Button();
+            this.btnIsEmptyGraph = new System.Windows.Forms.Button();
+            this.btnIsZeroGraph = new System.Windows.Forms.Button();
+            this.btnIsRegularGraph = new System.Windows.Forms.Button();
+            this.btnIsStrongConectedGraph = new System.Windows.Forms.Button();
+            this.btnAdjacencyMatrixView = new System.Windows.Forms.Button();
+            this.btnIncidenceMatrixView = new System.Windows.Forms.Button();
+            this.graphComponent1 = new GraphLib.GraphComponent(this.components);
+            this.btnInitiGraph = new System.Windows.Forms.Button();
+            this.btnFindShortestPath = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnIsConnectedGraph
+            // 
+            this.btnIsConnectedGraph.Location = new System.Drawing.Point(933, 21);
+            this.btnIsConnectedGraph.Name = "btnIsConnectedGraph";
+            this.btnIsConnectedGraph.Size = new System.Drawing.Size(222, 46);
+            this.btnIsConnectedGraph.TabIndex = 1;
+            this.btnIsConnectedGraph.Text = "IsConnectedGraph";
+            this.btnIsConnectedGraph.UseVisualStyleBackColor = true;
+            this.btnIsConnectedGraph.Click += new System.EventHandler(this.btnIsConnectedGraph_Click);
+            // 
+            // btnIsEmptyGraph
+            // 
+            this.btnIsEmptyGraph.Location = new System.Drawing.Point(933, 73);
+            this.btnIsEmptyGraph.Name = "btnIsEmptyGraph";
+            this.btnIsEmptyGraph.Size = new System.Drawing.Size(222, 46);
+            this.btnIsEmptyGraph.TabIndex = 2;
+            this.btnIsEmptyGraph.Text = "IsEmptyGraph";
+            this.btnIsEmptyGraph.UseVisualStyleBackColor = true;
+            this.btnIsEmptyGraph.Click += new System.EventHandler(this.btnIsEmptyGraph_Click);
+            // 
+            // btnIsZeroGraph
+            // 
+            this.btnIsZeroGraph.Location = new System.Drawing.Point(933, 125);
+            this.btnIsZeroGraph.Name = "btnIsZeroGraph";
+            this.btnIsZeroGraph.Size = new System.Drawing.Size(222, 46);
+            this.btnIsZeroGraph.TabIndex = 3;
+            this.btnIsZeroGraph.Text = "IsZeroGraph";
+            this.btnIsZeroGraph.UseVisualStyleBackColor = true;
+            this.btnIsZeroGraph.Click += new System.EventHandler(this.btnIsZeroGraph_Click);
+            // 
+            // btnIsRegularGraph
+            // 
+            this.btnIsRegularGraph.Location = new System.Drawing.Point(933, 177);
+            this.btnIsRegularGraph.Name = "btnIsRegularGraph";
+            this.btnIsRegularGraph.Size = new System.Drawing.Size(222, 46);
+            this.btnIsRegularGraph.TabIndex = 4;
+            this.btnIsRegularGraph.Text = "IsRegularGraph";
+            this.btnIsRegularGraph.UseVisualStyleBackColor = true;
+            this.btnIsRegularGraph.Click += new System.EventHandler(this.btnIsRegularGraph_Click);
+            // 
+            // btnIsStrongConectedGraph
+            // 
+            this.btnIsStrongConectedGraph.Location = new System.Drawing.Point(933, 229);
+            this.btnIsStrongConectedGraph.Name = "btnIsStrongConectedGraph";
+            this.btnIsStrongConectedGraph.Size = new System.Drawing.Size(222, 46);
+            this.btnIsStrongConectedGraph.TabIndex = 5;
+            this.btnIsStrongConectedGraph.Text = "IsStrongConectedGraph";
+            this.btnIsStrongConectedGraph.UseVisualStyleBackColor = true;
+            this.btnIsStrongConectedGraph.Click += new System.EventHandler(this.btnIsStrongConectedGraph_Click);
+            // 
+            // btnAdjacencyMatrixView
+            // 
+            this.btnAdjacencyMatrixView.Location = new System.Drawing.Point(933, 337);
+            this.btnAdjacencyMatrixView.Name = "btnAdjacencyMatrixView";
+            this.btnAdjacencyMatrixView.Size = new System.Drawing.Size(222, 46);
+            this.btnAdjacencyMatrixView.TabIndex = 6;
+            this.btnAdjacencyMatrixView.Text = "AdjacencyMatrixView";
+            this.btnAdjacencyMatrixView.UseVisualStyleBackColor = true;
+            this.btnAdjacencyMatrixView.Click += new System.EventHandler(this.btnAdjacencyMatrixView_Click);
+            // 
+            // btnIncidenceMatrixView
+            // 
+            this.btnIncidenceMatrixView.Location = new System.Drawing.Point(933, 389);
+            this.btnIncidenceMatrixView.Name = "btnIncidenceMatrixView";
+            this.btnIncidenceMatrixView.Size = new System.Drawing.Size(222, 46);
+            this.btnIncidenceMatrixView.TabIndex = 10;
+            this.btnIncidenceMatrixView.Text = "IncidenceMatrixView";
+            this.btnIncidenceMatrixView.UseVisualStyleBackColor = true;
+            this.btnIncidenceMatrixView.Click += new System.EventHandler(this.btnIncidenceMatrixView_Click);
+            // 
+            // graphComponent1
+            // 
+            this.graphComponent1.ActiveEditingButtons = true;
+            this.graphComponent1.AllowUserToResizeColumns = false;
+            this.graphComponent1.AllowUserToResizeRows = false;
+            this.graphComponent1.BackgroundColor = System.Drawing.SystemColors.AppWorkspace;
+            this.graphComponent1.GirdBorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.graphComponent1.GraphView = GraphLib.GraphView.IncidenceMatrix;
+            this.graphComponent1.GridColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.graphComponent1.Location = new System.Drawing.Point(12, 12);
+            this.graphComponent1.Name = "graphComponent1";
+            this.graphComponent1.ReadOnlyGrid = false;
+            this.graphComponent1.Size = new System.Drawing.Size(915, 499);
+            this.graphComponent1.TabIndex = 12;
+            this.graphComponent1.VertexName = "a";
+            // 
+            // btnInitiGraph
+            // 
+            this.btnInitiGraph.Location = new System.Drawing.Point(933, 281);
+            this.btnInitiGraph.Name = "btnInitiGraph";
+            this.btnInitiGraph.Size = new System.Drawing.Size(222, 46);
+            this.btnInitiGraph.TabIndex = 13;
+            this.btnInitiGraph.Text = "Init Graph";
+            this.btnInitiGraph.UseVisualStyleBackColor = true;
+            this.btnInitiGraph.Click += new System.EventHandler(this.btnInitiGraph_Click);
+            // 
+            // btnFindShortestPath
+            // 
+            this.btnFindShortestPath.Location = new System.Drawing.Point(933, 441);
+            this.btnFindShortestPath.Name = "btnFindShortestPath";
+            this.btnFindShortestPath.Size = new System.Drawing.Size(222, 46);
+            this.btnFindShortestPath.TabIndex = 14;
+            this.btnFindShortestPath.Text = "FindShortestPath";
+            this.btnFindShortestPath.UseVisualStyleBackColor = true;
+            this.btnFindShortestPath.Click += new System.EventHandler(this.btnFindShortestPath_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1159, 523);
+            this.Controls.Add(this.btnFindShortestPath);
+            this.Controls.Add(this.btnInitiGraph);
+            this.Controls.Add(this.graphComponent1);
+            this.Controls.Add(this.btnIncidenceMatrixView);
+            this.Controls.Add(this.btnAdjacencyMatrixView);
+            this.Controls.Add(this.btnIsStrongConectedGraph);
+            this.Controls.Add(this.btnIsRegularGraph);
+            this.Controls.Add(this.btnIsZeroGraph);
+            this.Controls.Add(this.btnIsEmptyGraph);
+            this.Controls.Add(this.btnIsConnectedGraph);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnIsConnectedGraph;
+        private System.Windows.Forms.Button btnIsEmptyGraph;
+        private System.Windows.Forms.Button btnIsZeroGraph;
+        private System.Windows.Forms.Button btnIsRegularGraph;
+        private System.Windows.Forms.Button btnIsStrongConectedGraph;
+        private System.Windows.Forms.Button btnAdjacencyMatrixView;
+        private System.Windows.Forms.Button btnIncidenceMatrixView;
+        private GraphLib.GraphComponent graphComponent1;
+        private System.Windows.Forms.Button btnInitiGraph;
+        private System.Windows.Forms.Button btnFindShortestPath;
+    }
+}
+
---
+++ TestGraphComponentDLL/TestGraphComponentDLL/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GraphLib;
+
+namespace TestGraphComponentDLL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnIsConnectedGraph_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(graphComponent1.ToGraph().IsConnectedGraph.ToString());
+        }
+
+        private void btnIsEmptyGraph_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(graphComponent1.ToGraph().IsEdgesEmpty.ToString());
+        }
+
+        private void btnIsZeroGraph_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(graphComponent1.ToGraph().IsZeroGraph.ToString());
+        }
+
+        private void btnIsRegularGraph_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(graphComponent1.ToGraph().IsRegularGraph.ToString());
+        }
+
+        private void btnIsStrongConectedGraph_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(graphComponent1.ToGraph().IsStrongConectedGraph.ToString());
+        }
+
+
+        private void btnAdjacencyMatrixView_Click(object sender, EventArgs e)
+        {
+            graphComponent1.GraphView = GraphView.AdjacencyMatrix;
+        }
+
+        private void btnIncidenceMatrixView_Click(object sender, EventArgs e)
+        {
+            graphComponent1.GraphView = GraphView.IncidenceMatrix;
+        }
+
+        private void btnInitiGraph_Click(object sender, EventArgs e)
+        {
+            int[][] matrix =
+            {
+                new int[]{0, 1, 0, 0, 0, 0, 1, 1, 0, 0 },
+                new int[]{1, 0, 1, 0, 0, 0, 0, 1, 0, 0 },
+                new int[]{0, 1, 0, 0, 0, 0, 0, 0, 0, 1 },
+                new int[]{0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
+                new int[]{0, 0, 0, 1, 0, 0, 0, 0, 1, 0 },
+                new int[]{0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+                new int[]{1, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+                new int[]{1, 1, 0, 0, 0, 0, 0, 0, 1, 0 },
+                new int[]{0, 0, 0, 0, 1, 1, 1, 1, 0, 1 },
+                new int[]{0, 0, 1, 1, 0, 0, 0, 0, 1, 0 }
+                };
+
+            string vertexName = "vertex";
+            string edgeName = "edge";
+            Graph graph = new Graph(matrix, false, vertexName, edgeName);
+            graphComponent1.FromGraph(graph);
+            
+        }
+
+        private void btnFindShortestPath_Click(object sender, EventArgs e)
+        {
+            //Graph graph = graphComponent1.ToGraph();
+
+            //// initialization weight values  /////////////////
+
+            //graph[0].IncidentalEdges(graph[1]).First().Weight = 2;
+            //graph[0].IncidentalEdges(graph[7]).First().Weight = 3;
+            //graph[0].IncidentalEdges(graph[6]).First().Weight = 4;
+            //graph[1].IncidentalEdges(graph[7]).First().Weight = 1;
+            //graph[1].IncidentalEdges(graph[2]).First().Weight = 7;
+            //graph[2].IncidentalEdges(graph[9]).First().Weight = 4;
+            //graph[3].IncidentalEdges(graph[9]).First().Weight = 15;
+            //graph[3].IncidentalEdges(graph[4]).First().Weight = 13;
+            //graph[4].IncidentalEdges(graph[8]).First().Weight = 3;
+            //graph[5].IncidentalEdges(graph[8]).First().Weight = 1;
+            //graph[6].IncidentalEdges(graph[8]).First().Weight = 7;
+            //graph[7].IncidentalEdges(graph[8]).First().Weight = 18;
+            //graph[8].IncidentalEdges(graph[9]).First().Weight = 12;
+
+            
+            //List<Vertex> path = graph.FindShortestPath(7, 3);
+            
+            //StringBuilder sb = new StringBuilder();
+            //foreach(Vertex v in path)
+            //{
+            //    sb.Append(v.Name).Append("\n");
+            //}
+
+            //MessageBox.Show(sb.ToString());
+
+            TestOrientedGraphShortestPath();
+
+        }
+
+        private void TestOrientedGraphShortestPath()
+        {
+            int[][] matrix = 
+            {
+                new int[] { 0, 0, 0, 0, 0, 0, 1, 1, 0, 0 },
+                new int[] { 1, 0, 1, 0, 0, 0, 0, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                new int[] { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                new int[] { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0, 1, 0, 1, 1, 0, 0 },
+                new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 } 
+            };
+
+            string vertexName = "vertex";
+            string edgeName = "edge";
+            Graph graph = new Graph(matrix, true, vertexName, edgeName);
+            graphComponent1.FromGraph(graph);
+
+            List<Vertex> path = graph.FindShortestPath(7, 3);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (Vertex v in path)
+            {
+                sb.Append(v.Name).Append("\n");
+            }
+
+            MessageBox.Show(sb.ToString());
+        }
+    }
+}
